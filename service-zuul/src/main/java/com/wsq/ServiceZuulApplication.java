b13c96fa97b985a56d1c763bd7b19ed2fb1515f8
WangShaoqiang <WangShaoqiang@huazhu.com>
2020-05-08 16:39:30 +0800

    添加zuul模块控制路由和过滤器

---
+++ service-zuul/src/main/java/com/wsq/ServiceZuulApplication.java
@@ -0,0 +1,22 @@
+package com.wsq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author wsq
+ * @version 1.0
+ * @date 2020/5/8 15:59
+ */
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class ServiceZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run( ServiceZuulApplication.class, args );
+    }
+}
